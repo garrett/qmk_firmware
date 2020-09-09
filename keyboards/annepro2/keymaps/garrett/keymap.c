8155c55a87a242caf15f2117941c8d467038e4f2
Garrett LeSage <garrett@lesage.us>
2020-09-09 11:01:04 +0200

    Add additional arrow keys on nav layer; magic fn; special esc

--- keyboards/annepro2/keymaps/garrett/keymap.c
+++ keyboards/annepro2/keymaps/garrett/keymap.c
@@ -13,7 +13,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*
   * Layer _BASE_LAYER
   * ,-----------------------------------------------------------------------------------------.
-  * |  `  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
+  * | `esc|  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
   * |-----------------------------------------------------------------------------------------+
   * | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
   * |-----------------------------------------------------------------------------------------+
@@ -33,15 +33,15 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |-----------------------------------------------------------------------------------------+
   * |            |     |     |     |     |     |     |     |     |     |     |                |
   * |-----------------------------------------------------------------------------------------+
-  * |       |       |       |                                 |       | FN_L  |       |       |
+  * |       |       |       |             Arrow_L             |       | FN_L  |       |       |
   * \-----------------------------------------------------------------------------------------/
   */
   [_BASE_LAYER] = KEYMAP( /* Base */
-    KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,   KC_BSPC,
+    KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,   KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,  KC_BSLS,
     LT(_ARROW_LAYER, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,         KC_ENT,
-    KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-    KC_LCTL,KC_LGUI, KC_LALT,      LT(_ARROW_LAYER, KC_SPC),             KC_RALT,  MO(_FN_LAYER),  KC_RGUI, KC_RCTL
+    KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
+    KC_LCTL,KC_LGUI, KC_LALT,      LT(_ARROW_LAYER, KC_SPC),             KC_RALT,  LT(MO(_FN_LAYER),KC_LEFT),  LT(KC_RGUI,KC_DOWN), RCTL_T(KC_RGHT)
   ),
   /*
   * Layer _MAC_LAYER
@@ -78,23 +78,23 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   /* Layer _ARROW_LAYER
   * ,-----------------------------------------------------------------------------------------.
-  * |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
+  * |  `   |     |     |     |     |     |     |     | ▶⏸ |  ⏮  |  ⏭  |     |     |   PrScr   |
   * |-----------------------------------------------------------------------------------------+
-  * |        |     |     |     |     |     |     |     |     |     |     | Home| End |        |
+  * |  Home  | PGU |  ↑  | PGD | End |     |     |     |     |     |     | Home| End |        |
   * |-----------------------------------------------------------------------------------------+
-  * |         |     |     |     |     |     | <-  | \/  |  ^  | ->  | PGU |     |             |
+  * |         |  ←  |  ↓  |  →  |     |     |  ←  |  ↓  |  ↑  |  →  | PGU | PGD |             |
   * |-----------------------------------------------------------------------------------------+
-  * |            |     |     |     |     |     |     |     |     | Ins | Del |                |
+  * |            | Mute | V-DWN | V-Up |     |     |     |     |     | Ins | Del |            |
   * |-----------------------------------------------------------------------------------------+
   * |       |       |       |                                 |       |       |       |       |
   * \-----------------------------------------------------------------------------------------/
   */
   [_ARROW_LAYER] = KEYMAP( /* Base */
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,
-    KC_HOME, KC_PGUP, KC_UP, KC_PGDN, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_PGUP, KC_PGDN,         KC_TRNS,
-    KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INSERT, KC_DELETE, KC_TRNS,
-    KC_TRNS,KC_TRNS, KC_TRNS,      KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
+    KC_GRAVE, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______,   KC_PSCR,
+    KC_HOME, KC_PGUP, KC_UP, KC_PGDN, KC_END, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______,
+    _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_PGUP, KC_PGDN,         _______,
+    _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, KC_INSERT, KC_DELETE, _______,
+    _______,_______, _______,      _______,             _______,  _______,  _______, _______
   ),
   /* Layer _FN_LAYER
   * ,-----------------------------------------------------------------------------------------.
@@ -110,11 +110,11 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * \-----------------------------------------------------------------------------------------/
   */
   [_FN_LAYER] = KEYMAP( /* Base */
-    KC_AP2_USB, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,   KC_TRNS,
-    KC_AP2_BT_UNPAIR, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_AP_LED_ON,
-    KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_PGUP, KC_PGDN,         KC_AP_LED_OFF,
-    KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INSERT, KC_DELETE, KC_TRNS,
-    KC_TRNS,KC_TRNS, KC_TRNS,      KC_TRNS,             KC_TRNS,  KC_TRNS,  DF(_MAC_LAYER), DF(_BASE_LAYER)
+    KC_AP2_USB, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,   _______,
+    KC_AP2_BT_UNPAIR, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_AP_LED_ON,
+    KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_PGUP, KC_PGDN,         KC_AP_LED_OFF,
+    _______,_______, _______, _______, _______, _______, _______, _______, _______, KC_INSERT, KC_DELETE, _______,
+    _______,_______, _______,      _______,             _______,  _______,  DF(_MAC_LAYER), DF(_BASE_LAYER)
   ),
 };
 
